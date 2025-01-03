54e66335dc5d9602bdcac410d793fbd2752df9b5
Faiz Shaikh <56632540+faizshaikh-org@users.noreply.github.com>
2025-01-03 17:44:44 +0530

    Connection string changes
    Connection string changes

--- learningapp/Program.cs
+++ learningapp/Program.cs
@@ -2,9 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Configuration
-.AddJsonFile("appsettings.json")
-    .AddEnvironmentVariables();
+builder.Configuration.AddAzureAppConfiguration("Endpoint=https://applicationconfig0001.azconfig.io;Id=b5oI;Secret=DwahWyYyH27n2UVuuMNGDK0Zl7UJjI8xu7WnneFBYdjAZ1FjBYKVJQQJ99BAACGhslB5oe4qAAABAZAC6VhY");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
